33b07a870350c2ef3c066d36a6283d1809c94562
SherifShokry <sshokry@bluecrunch.com>
2021-10-29 20:58:00 +0200

    Create the FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  EssentialFeed
+//
+//  Created by SherifShokry on 29/10/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id : UUID
+    let description : String?
+    let location : String?
+    let imageURL : URL
+
+}
